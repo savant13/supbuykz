47f1cdd1785b7f77f573e71e28f46f6d1ca97ad3
savant13 <190103292@stu.sdu.edu.kz>
2023-06-25 22:34:39 +0600

    sdf

--- backend/product/models.py
+++ backend/product/models.py
@@ -6,13 +6,15 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=200, blank=False, null=False)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=0)
     stock = models.BooleanField(default=False)
     image = models.ImageField(null=True, blank=True)
     category = models.CharField(null=True,default=None,max_length=40)
     type_product = models.CharField(max_length=10,default='B')
     owner = models.ForeignKey(User,on_delete=models.CASCADE,default=None,null=True)
     count = models.IntegerField(default=1)
+
+
     def __str__(self):
         return self.name
     
